8a87c032687a1534f81e373c0d28f39b07b89abb
emily <emily.s23@aluno.ifsc.edu.br>
2025-08-29 08:53:05 -0300

    TelaCredenciais
    JPanel

---
+++ TelasCashly/src/telas/TelaCredenciais.java
@@ -0,0 +1,44 @@
+package telas;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+
+public class TelaCredenciais extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private TelaLogin2 contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaCredenciais frame = new TelaCredenciais();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaCredenciais() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new TelaLogin2();
+		contentPane.setBackground(new Color(216, 178, 184));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
