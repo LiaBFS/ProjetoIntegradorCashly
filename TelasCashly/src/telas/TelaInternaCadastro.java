aa44b4e2074dde10a7878447f2cac3b66f616ae5
Lia <lia.bfs@aluno.ifsc.edu.br>
2025-08-29 09:33:23 -0300

    TelaInternaCadastro

---
+++ TelasCashly/src/telas/TelaInternaCadastro.java
@@ -0,0 +1,16 @@
+package telas;
+
+import javax.swing.JPanel;
+
+public class TelaInternaCadastro extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public TelaInternaCadastro() {
+
+	}
+
+}
