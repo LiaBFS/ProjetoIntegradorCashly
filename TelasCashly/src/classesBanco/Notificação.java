49f5148cdd26e905279a5b3abf8e0365b7afd252
Isabella <isabella.u@aluno.ifsc.edu.br>
2025-09-02 11:10:12 -0300

    DAOs

--- TelasCashly/src/classesBanco/Notificação.java
+++ TelasCashly/src/classesBanco/Notificação.java
@@ -1,5 +1,12 @@
 package classesBanco;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import java.util.Date;
 
 public class Notificação {
