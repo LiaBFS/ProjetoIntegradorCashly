25f2377646363d4b2d497bdc3bfaf812481a22e3
Emily <emily.s23@aluno.ifsc.edu.br>
2025-10-29 10:33:21 -0300

    kjsdbfiuwehjbgiw

--- TelasCashly/src/view/TelaInternaLancamentos.java
+++ TelasCashly/src/view/TelaInternaLancamentos.java
@@ -1,6 +1,9 @@
 package view;
 
 import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+
 import javax.swing.*;
 import javax.swing.border.*;
 import net.miginfocom.swing.MigLayout;
@@ -22,7 +25,7 @@ public class TelaInternaLancamentos extends JDialog {
 
 	public TelaInternaLancamentos() {
 		setTitle("Adicionar Lançamento");
-		setBounds(100, 100, 674, 515);
+		setBounds(100, 100, 674, 676);
 		setLocationRelativeTo(null);
 		setModal(true);
 
@@ -32,32 +35,32 @@ public class TelaInternaLancamentos extends JDialog {
 				new LineBorder(new Color(214, 199, 185), 3, true), // borda arredondada
 				new EmptyBorder(15, 15, 15, 15)));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		contentPanel.setLayout(new MigLayout("", "[grow][]", "[20px,grow][grow][grow][grow][grow]"));
+		contentPanel.setLayout(new MigLayout("", "[grow][]", "[grow][][grow][][grow][][grow][grow]"));
 
 		// 🔹 Título
-		JLabel lblTitulo = new JLabel("Adicionar Lançamento", SwingConstants.CENTER);
-		lblTitulo.setFont(new Font("Dialog", Font.BOLD, 29));
-		lblTitulo.setForeground(new Color(80, 50, 40));
-		contentPanel.add(lblTitulo, "cell 0 0 3 1,alignx center,aligny top");
+		JLabel lblAdicionarLancamento = new JLabel("Adicionar Lançamento", SwingConstants.CENTER);
+		lblAdicionarLancamento.setFont(new Font("Dialog", Font.BOLD, 35));
+		lblAdicionarLancamento.setForeground(new Color(80, 50, 40));
+		contentPanel.add(lblAdicionarLancamento, "cell 0 0 3 1,alignx center,aligny top");
 		
 		JPanelComBackground panel_2 = new JPanelComBackground("/imgs/NomeLançamento.png");
 		panel_2.setBackground(new Color(255, 245, 234));
-		contentPanel.add(panel_2, "cell 0 1 2 1,grow");
-		panel_2.setLayout(new MigLayout("", "[]", "[]"));
+		contentPanel.add(panel_2, "cell 0 2 2 1,grow");
+		panel_2.setLayout(new MigLayout("", "[]", "[10px,grow]"));
 		
 		JPanelComBackground panel_3 = new JPanelComBackground("/imgs/CategoriaLançamento.png");
 		panel_3.setBackground(new Color(255, 245, 234));
-		contentPanel.add(panel_3, "cell 0 2 2 1,grow");
-		panel_3.setLayout(new MigLayout("", "[]", "[]"));
+		contentPanel.add(panel_3, "cell 0 4 2 1,grow");
+		panel_3.setLayout(new MigLayout("", "[]", "[10px,grow]"));
 		
 		JPanelComBackground panel_4 = new JPanelComBackground("/imgs/DataLançamento.png");
 		panel_4.setBackground(new Color(255, 245, 234));
-		contentPanel.add(panel_4, "cell 0 3 2 1,grow");
-		panel_4.setLayout(new MigLayout("", "[]", "[]"));
+		contentPanel.add(panel_4, "cell 0 6 2 1,grow");
+		panel_4.setLayout(new MigLayout("", "[146][146]", "[10px,grow]"));
 		
 		JButton btnCancelar = new JButton("");
 		btnCancelar.setIcon(new ImageIcon(TelaInternaLancamentos.class.getResource("/imgs/CancelarLançamento.png")));
-		contentPanel.add(btnCancelar, "flowx,cell 1 4");
+		contentPanel.add(btnCancelar, "flowx,cell 1 7");
 		btnCancelar.setOpaque(false);
 		btnCancelar.setFocusPainted(false);
 		btnCancelar.setContentAreaFilled(false);
@@ -65,7 +68,7 @@ public class TelaInternaLancamentos extends JDialog {
 		
 		JButton btnAdicionar = new JButton("");
 		btnAdicionar.setIcon(new ImageIcon(TelaInternaLancamentos.class.getResource("/imgs/AdicionarLançamento.png")));
-		contentPanel.add(btnAdicionar, "cell 1 4");
+		contentPanel.add(btnAdicionar, "cell 1 7");
 		btnAdicionar.setOpaque(false);
 		btnAdicionar.setFocusPainted(false);
 		btnAdicionar.setContentAreaFilled(false);
