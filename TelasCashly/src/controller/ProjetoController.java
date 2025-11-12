a4bff5c63ec278f69078b0b02080d9c36b318cb1
Unknown <unknown@example.com>
2025-11-12 11:41:39 -0300

    fazer tela relatorio e controllerrelatorio, se der twmpo faco hj em casa

--- TelasCashly/src/controller/ProjetoController.java
+++ TelasCashly/src/controller/ProjetoController.java
@@ -21,7 +21,7 @@ public class ProjetoController {
 		this.projetoDAO = projetoDAO;
 		
 		this.tela.getBtnAdicionarLancamento().addActionListener(e -> abrirPopUpLancamento());
-		this.tela.getBtnIrGraficos().addActionListener(e -> abrirTelaEscolhaGrafico());
+		//this.tela.getBtnIrGraficos().addActionListener(e -> abrirTelaEscolhaGrafico());
 		
 	}
 
